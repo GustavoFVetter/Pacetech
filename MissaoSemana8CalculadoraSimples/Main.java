eaa28e581a433cb76973d5bcb964652c4c245b92
GustavoFVetter <92186356+GustavoFVetter@users.noreply.github.com>
2021-11-17 10:42:32 -0300

    Missão Sem. 8 - Calculadora java
    Calculadora java rodando em interface gráfica.

---
+++ MissaoSemana8CalculadoraSimples/Main.java
@@ -0,0 +1,19 @@
+
+package MissaoSemana8CalculadoraSimples;
+
+/**
+  * @author Gustavo F. Vetter 13/11/2021
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
